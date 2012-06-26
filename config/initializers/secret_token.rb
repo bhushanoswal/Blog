4e79de1fa7cfb1e57ac2227d946952a221ecb4d4
webonise <webonise@webonise.(none)>
2012-06-26 12:01:27 +0530

    Application Structure created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '85b173553b920c43e644067dadc86852017f4ae643b2e4f6422a7840bbd2fc061c4b1a060d46bf2b2c20b07f0558bf515963a261b1e0e93ec816da41d5274a05'
